234b786efdc2f0fe9e72254837757238390ad25b
fatihakce35 <fatih.akce35@gmail.com>
2023-03-18 22:21:23 +0300

    files seperated by folder structure

--- app.js
+++ app.js
@@ -4,9 +4,26 @@ const app = express()
 const port = 3000
 
 
+//template engine
+app.set("view engine", "ejs")
+
+
+//static files
+app.use(express.static('public'))
+
+
+
+
 app.get("/", (req, res) => {
-    res.send("New App !")
+    res.render("index")
 })
+
+
+// rendering about page
+app.get("/about", (req, res) => {
+    res.render("about")
+})
+
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`)
